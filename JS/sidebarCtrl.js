fdec09476fb72f92df92fcdf8fc6b8e99a2ba34f
Lucas Rodrigues <rod.luk@gmail.com>
2017-04-11 18:45:19 +0200

    Firebase

--- JS/sidebarCtrl.js
+++ JS/sidebarCtrl.js
@@ -1,4 +1,13 @@
-eventPlannerApp.controller('sidebarCtrl', function ($scope,$routeParams) {
+eventPlannerApp.controller('sidebarCtrl', function ($scope,$routeParams, $firebaseObject, $firebaseArray) {
 
-
+	//Initializing the 
+	firebase.auth().onAuthStateChanged(function(user) {
+	  if (user) {
+	    var ref = firebase.database().ref("events");
+		var uid = firebase.auth().currentUser.uid;
+	 	var syncObject = $firebaseObject(ref.child(uid));
+	 	syncObject.$bindTo($scope, "events");
+	  } 
+	});
+ 
 }); 
