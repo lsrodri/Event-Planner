9f7c540cb848923cf814ccde1accb8972daf7e4d
Lucas Rodrigues <rod.luk@gmail.com>
2017-04-12 17:26:36 +0200

    added a header

--- JS/headerCtrl.js
+++ JS/headerCtrl.js
@@ -1,4 +1,10 @@
 eventPlannerApp.controller('headerCtrl', function ($scope,$routeParams) {
 
+	firebase.auth().onAuthStateChanged(function(user) {
+	  if (user) {
+	    $scope.name = firebase.auth().currentUser.displayName;
+		$scope.avatar = firebase.auth().currentUser.photoURL;
+	  }
+	});
 
 }); 
